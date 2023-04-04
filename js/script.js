d3c55556006f2df5d3321f67126420afdaeb7186
Marcin Miekisz <miekiszmarcin@gmail.com>
2023-04-04 17:03:02 +0200

    add playRound() function

--- js/script.js
+++ js/script.js
@@ -10,5 +10,20 @@ function getComputerChoice() {
     return moves[randomNumber];
 }
 
+function filterPlayerSelection(playerSelection) {
+    const playerSelectionFiltered = playerSelection.toLowerCase().charAt(0).toUpperCase() + playerSelection.toLowerCase().slice(1);
+    return playerSelectionFiltered;
+}
 
-
+function playRound(playerSelection, computerSelection) {
+    const player = filterPlayerSelection(playerSelection);
+    let message = "";
+    if (player === "Rock" && computerSelection === "Scissors" || player === "Scissors" && computerSelection === "Paper" || player === "Paper" && computerSelection === "Rock") message += `You win! ${player} beats ${computerSelection}`;
+    else if (player === "Rock" && computerSelection === "Paper" || player === "Scissors" && computerSelection === "Rock" || player === "Paper" && computerSelection === "Scissors") message += `You lose! ${computerSelection} beats ${player}`;
+    else return `It's a tie! You both picked ${player}`;
+    return message;
+}   
+  const playerSelection = "rock";
+  const computerSelection = getComputerChoice();
+  console.log(computerSelection);
+  console.log(playRound(playerSelection, computerSelection));
