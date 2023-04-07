6bb0e4245b5c52d755c0463697825012fbea16c2
Marcin Miekisz <miekiszmarcin@gmail.com>
2023-04-07 09:21:18 +0200

    add game method and scores

--- js/script.js
+++ js/script.js
@@ -18,12 +18,38 @@ function filterPlayerSelection(playerSelection) {
 function playRound(playerSelection, computerSelection) {
     const player = filterPlayerSelection(playerSelection);
     let message = "";
-    if (player === "Rock" && computerSelection === "Scissors" || player === "Scissors" && computerSelection === "Paper" || player === "Paper" && computerSelection === "Rock") message += `You win! ${player} beats ${computerSelection}`;
-    else if (player === "Rock" && computerSelection === "Paper" || player === "Scissors" && computerSelection === "Rock" || player === "Paper" && computerSelection === "Scissors") message += `You lose! ${computerSelection} beats ${player}`;
-    else return `It's a tie! You both picked ${player}`;
+    if (player === "Rock" && computerSelection === "Scissors" || player === "Scissors" && computerSelection === "Paper" || player === "Paper" && computerSelection === "Rock") {
+        message += `You win! ${player} beats ${computerSelection}`;
+        score.player++;
+    } else if (player === "Rock" && computerSelection === "Paper" || player === "Scissors" && computerSelection === "Rock" || player === "Paper" && computerSelection === "Scissors") {
+        message += `You lose! ${computerSelection} beats ${player}`;
+        score.computer++;
+    } else return `It's a tie! You both picked ${player}`;
+
     return message;
-}   
-  const playerSelection = "rock";
-  const computerSelection = getComputerChoice();
-  console.log(computerSelection);
-  console.log(playRound(playerSelection, computerSelection));
+}
+
+const score = {
+    player: 0,
+    computer: 0
+}
+
+let round = 1;
+function game() {
+    while (round <= 5) {
+        // Prompt user for an input
+        const playerSelection = prompt("What is your pick?");
+        const computerSelection = getComputerChoice();
+        // Play game
+        console.log(playRound(playerSelection, computerSelection));
+        console.log(`Player: ${score.player} Computer: ${score.computer}`);
+        round++;
+    // add score
+    // loop through it 5 times
+    // show the final score
+    }
+}
+
+game();
+let final = score.player > score.computer ? "You won the game!" : "You lost the game :(";
+console.log(final)
